53d3cfa3e8a74fcb71399d1cb97d47225e281524
hyang <hyoungcn@outlook.com>
2017-03-26 20:34:54 +0800

    dynamic proxy

--- src/main/java/com/hyang/jdk/proxy/run/DynamicGenerator.java
+++ src/main/java/com/hyang/jdk/proxy/run/DynamicGenerator.java
@@ -18,21 +18,25 @@ public class DynamicGenerator {
      * @param args
      */
     public static void main(String[] args) {
-        //创建中介类实例
+        //1.创建中介类实例
         DynamicProxy dynamicProxy = new DynamicProxy(new Vendor());
-        //会产生一个$Proxy0.class文件，这个文件就是动态生成的代理类文件。
-        System.getProperties().put("sun.misc.ProxyGeneratory.saveGeneratedFiles", "true");
-        //获取代理类实例sell
-        /*
-        static Object newProxyInstance(ClassLoader loader, Class<?>[] interfaces, InvocationHandler h) throws IllegalArgumentException
 
-        方法的三个参数含义分别如下：
-        loader：定义了代理类的ClassLoder；
-        interfaces：代理类实现的接口列表
-        h：调用处理器，也就是我们上面定义的实现了InvocationHandler接口的类实例
+        //2.会产生一个$Proxy0.class文件，这个文件就是动态生成的代理类文件。
+        //相当于人家自动给你实现了一个代理类的class（而不用手动实现代理类）
+        System.getProperties().put("sun.misc.ProxyGeneratory.saveGeneratedFiles", "true");
+
+        //3.获取代理类实例sell
+        /*
+            static Object newProxyInstance(ClassLoader loader, Class<?>[] interfaces, InvocationHandler h) throws IllegalArgumentException
+
+            方法的三个参数含义分别如下：
+            loader：定义了代理类的ClassLoder；
+            interfaces：代理类实现的接口列表
+            h：调用处理器，也就是我们定义的实现了InvocationHandler接口的类实例
          */
         Sell sell = (Sell) (Proxy.newProxyInstance(Sell.class.getClassLoader(), new Class[]{Sell.class}, dynamicProxy));
-        //通过代理类对象调用代理类方法，实际上会转到invoke方法里调用
+
+        //4.通过代理类对象调用代理类方法，实际上会转到invoke方法里调用
         sell.doSell();
         sell.doAd();
 
